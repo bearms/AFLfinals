a21d001e97324727f00b9610debe96c6282d1fd3
BjornRyff <36492303+bearms@users.noreply.github.com>
2023-09-13 14:54:58 +1000

    footy.py

--- footy.py
+++ footy.py
@@ -24,36 +24,36 @@ def play_game(team1, team2):
         return team2, team1
 # Round 1 the losers of the top 4 will play the winners in the bottom 4 and the losers in bottom 4 are knocked out
 print("Round 1 Lineup:")
-print("Collingwood vs Port Adelaide")
-print("Brisbane vs Melbourne")
+print("Collingwood vs Melbourne")
 print("Carlton vs Sydney")
 print("St Kilda vs GWS Giants")
+print("Brisbane vs Port Adelaide")
 input("Press Enter to start Round 1...")
 
-round_1_game_1_winner, round_1_game_1_loser = play_game('Collingwood', 'Port Adelaide')
-round_1_game_2_winner, round_1_game_2_loser = play_game('Brisbane', 'Melbourne')
-round_1_game_3_winner, round_1_game_3_loser = play_game('Carlton', 'Sydney')
-round_1_game_4_winner, round_1_game_4_loser = play_game('St Kilda', 'GWS Giants')
+round_1_game_1_winner, round_1_game_1_loser = play_game('Collingwood', 'Melbourne')
+round_1_game_2_winner, round_1_game_2_loser = play_game('Carlton', 'Sydney')
+round_1_game_3_winner, round_1_game_3_loser = play_game('St Kilda', 'GWS Giants')
+round_1_game_4_winner, round_1_game_4_loser = play_game('Brisbane', 'Port Adelaide')
 
 # The winner of the bottom 4 will play the losers of the top 4, the losers in the games are knocked out.
 
 print("\nRound 2 Lineup:")
-print(f"{round_1_game_3_winner} vs {round_1_game_2_loser}")
-print(f"{round_1_game_4_winner} vs {round_1_game_1_loser}")
+print(f"{round_1_game_2_winner} vs {round_1_game_1_loser}")
+print(f"{round_1_game_3_winner} vs {round_1_game_4_loser}")
 input("Press Enter to start Round 2...")
 
-round_2_game_1_winner, round_2_game_1_loser = play_game(round_1_game_3_winner, round_1_game_2_loser)
-round_2_game_2_winner, round_2_game_2_loser = play_game(round_1_game_4_winner, round_1_game_1_loser)
+round_2_game_1_winner, round_2_game_1_loser = play_game(round_1_game_2_winner, round_1_game_1_loser)
+round_2_game_2_winner, round_2_game_2_loser = play_game(round_1_game_3_winner, round_1_game_4_loser)
 
 # Preliminary finals are the winners of round 2 play the winners of the top 4, losers in the games are knocked out
 # And the winners go on to the finals.
 
 print("\nPreliminary Finals Lineup:")
-print(f"{round_2_game_1_winner} vs {round_1_game_2_winner}")
+print(f"{round_2_game_1_winner} vs {round_1_game_4_winner}")
 print(f"{round_2_game_2_winner} vs {round_1_game_1_winner}")
 input("Press Enter to start Preliminary Finals...")
 
-prelim_finals_winner, prelim_finals_loser = play_game(round_2_game_1_winner, round_1_game_2_winner)
+prelim_finals_winner, prelim_finals_loser = play_game(round_2_game_1_winner, round_1_game_4_winner)
 prelim_finals2_winner, prelim_finals2_loser = play_game(round_2_game_2_winner, round_1_game_1_winner)
 
 # AFL grand finals
